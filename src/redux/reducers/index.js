f5fdc6d01d869e950d2fe40b58b79c645002e4d6
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-11-06 17:31:18 +0100

    Commit 2

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,21 @@
-const initialState ={
-    favourites:{
-        content:[]
-    }
-}
+const initialState = {
+  favourites: {
+    content: [],
+  },
+};
 
-const mainReducer = ( state initialState )
+const mainReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_TO_FAVOURITES":
+      return {
+        ...state,
+        favourites: {
+          ...state.favourites,
+          content: [...state.favourites.content, action.payload],
+        },
+      };
+    default:
+      return state;
+  }
+};
+export default mainReducer;
