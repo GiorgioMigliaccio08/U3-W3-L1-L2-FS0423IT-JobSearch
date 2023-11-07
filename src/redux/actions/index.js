66f49950209cd010dbb2e3222b0a16f744e4da86
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-11-07 17:30:29 +0100

    Commit 3

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,10 +1,16 @@
 export const ADD_TO_FAVOURITE = "ADD_TO_FAVOURITE";
 export const REMOVE_FROM_FAVOURITE = "REMOVE_FROM_FAVOURITE";
 
+export const addToFavourite = (companyName) => {
+  return {
+    type: "ADD_TO_FAVOURITE",
+    payload: companyName,
+  };
+};
 
-export const addToCartFavourite = ( data.company_name)  => {
-    return {
-        type: "ADD_TO_FAVOURITE",
-        payload: data.company_name,
-    }
-}
+export const removeFromFavourite = (i) => {
+  return {
+    type: "REMOVE_FROM_FAVOURITE",
+    payload: i,
+  };
+};
